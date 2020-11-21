bc5262ce6dc8b2076847ca401210c26de8a5f981
M Ridho Hafidz <dosys18.04@gmail.com>
2020-11-21 13:17:43 +0700

    Add files via upload

--- learn-python/Tugas1_TI07_0110219051/Tugas1_TI07_0110219051.py
+++ learn-python/Tugas1_TI07_0110219051/Tugas1_TI07_0110219051.py
@@ -8,21 +8,21 @@ nim = input("Masukan NIM: ")
 kondisi = True
 #Perulangan variabel kondisi dengan tipe data Boolean [True or False]
 while kondisi:
-    #Menginput jumlah mata kuliah yang diambil akan melakukan perulangan, misalkan menginput 2 maka perulangan menjadi 2 kali
-    jmatkul = int(input("Berapa Jumlah Mata Kuliah yang diambil ? "))
+    #Menginput jumlah mata kuliah yang diambil untuk melakukan perulangan, misalkan menginput 2 maka perulangan menjadi 2 kali
+    jumlahmatkul = int(input("Berapa Jumlah Mata Kuliah yang diambil ? "))
     print("\n")
     #inisialisasi suatu variabel index dipakai setelah menginput jumlah mata kuliah
     i = 0
-    #inisialisasi suatu variabel total untuk menampung nilai total sks
-    total = 0
+    #inisialisasi suatu variabel totalsks untuk menampung nilai total sks
+    totalsks = 0
     #inisialisasi suatu variabel ip untuk menampung nilai total index prestasi
     ip = 0
     # melakukan validasi jumlah mata kuliah yang dimasukkan
-    if jmatkul < 1 or jmatkul > 8:
+    if jumlahmatkul < 1 or jumlahmatkul > 8:
         print("Jumlah Mata Kuliah antara 1 sampai 8")
     else:
         #melakukan perulangan kedua ketika kondisi index lebih kecil dari jumlah matkul
-        while i < jmatkul:
+        while i < jumlahmatkul:
             #index akan bertambah ketika melakukan perulangan
             i = i+1
             print("Nilai Mata Kuliah", i)
@@ -41,7 +41,7 @@ while kondisi:
             #menginput nilai uas
             uas = eval(input("Nilai UAS: "))
             #menjumlahkan nilai sks mulai dari mata kuliah pertama dan seterusnya dalam variabel total
-            total = total+sks   
+            totalsks = totalsks+sks   
             #melakukan penjumlahan bobot nilai untuk setiap mata kuliah dari masing-masing kuis, tugas, uts, dan uas        
             nilai = 0.15*kuis+0.15*tugas1+0.20*tugas2+0.25*uts+0.25*uas
             #untuk mencari nilai sesuai dengan variabel
@@ -73,6 +73,6 @@ print("Rangkuman")
 #Mencetak NIM
 print("NIM :", nim)
 #Mencetak Total SKS
-print("Total SKS :", total)
+print("Total SKS :", totalsks)
 #Mencetak nilai Index Prestasi dibagi total sks
-print("Index prestasi :", round(ip/total, 2))
+print("Index prestasi :", round(ip/totalsks, 2))
