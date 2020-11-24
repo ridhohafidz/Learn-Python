7223a7591117c76749692bfc5d839c42273d5504
M Ridho Hafidz <dosys18.04@gmail.com>
2020-11-24 10:05:29 +0700

    Add files via upload

--- learn-python/Tugas1_TI07_0110219051/Tugas1_TI07_0110219051.py
+++ learn-python/Tugas1_TI07_0110219051/Tugas1_TI07_0110219051.py
@@ -10,7 +10,6 @@ kondisi = True
 while kondisi:
     #Menginput jumlah mata kuliah yang diambil untuk melakukan perulangan, misalkan menginput 2 maka perulangan menjadi 2 kali
     jumlahmatkul = int(input("Berapa Jumlah Mata Kuliah yang diambil ? "))
-    print("\n")
     #inisialisasi suatu variabel index dipakai setelah menginput jumlah mata kuliah
     i = 0
     #inisialisasi suatu variabel totalsks untuk menampung nilai total sks
@@ -20,8 +19,10 @@ while kondisi:
     # melakukan validasi jumlah mata kuliah yang dimasukkan
     if jumlahmatkul < 1 or jumlahmatkul > 8:
         print("Jumlah Mata Kuliah antara 1 sampai 8")
+        print("\n")
+    # Jika pengguna memasukan jumlah mata kuliah 1 sampai 8, jalankan percabangan else
     else:
-        #melakukan perulangan kedua ketika kondisi index lebih kecil dari jumlah matkul
+        #melakukan perulangan ketika kondisi index lebih kecil dari jumlah matkul
         while i < jumlahmatkul:
             #index akan bertambah ketika melakukan perulangan
             i = i+1
@@ -44,23 +45,27 @@ while kondisi:
             totalsks = totalsks+sks   
             #melakukan penjumlahan bobot nilai untuk setiap mata kuliah dari masing-masing kuis, tugas, uts, dan uas        
             nilai = 0.15*kuis+0.15*tugas1+0.20*tugas2+0.25*uts+0.25*uas
-            #untuk mencari nilai sesuai dengan variabel
-            if 85 <= nilai <= 100:
+            #untuk mencari index prestasi dan nilai sesuai dengan variabel nilai
+            if 85 <= nilai <= 100: #jika variabel nilai antara 85 sampai 100 maka buat variabel grade dengan value A
                 grade = "A"
-                #index prestasi
+                #index prestasi nya 4.0 dengan tipe data float
                 index = float(4.0)
-            elif 70 <= nilai < 85:
+            elif 70 <= nilai < 85: #lain jika variabel nilai antara 70 sampai 85 maka buat variabel grade dengan value B
                 grade = "B"
+                #index prestasi nya 3.0 dengan tipe data float
                 index = float(3.0)
-            elif 55 <= nilai < 70:
+            elif 55 <= nilai < 70: #lain jika variabel nilai antara 55 sampai 70 maka buat variabel grade dengan value C
                 grade = "C"
+                #index prestasi nya 2.0 dengan tipe data float
                 index = float(2.0)
-            elif 40 <= nilai < 55:
+            elif 40 <= nilai < 55: #lain jika variabel nilai antara 40 sampai 55 maka buat variabel grade dengan value D
                 grade = "D"
+                #index prestasi nya 1.0 dengan tipe data float
                 index = float(1.0)
-            elif nilai < 40:
+            elif nilai < 40: #lain jika variabel nilai kurang dari 40 maka buat variabel grade dengan value E
                 grade = "E"
-                index = float(0)
+                #index prestasi nya 0 dengan tipe data float
+                index = float(0.0)
             #mencetak nilai mata kuliah
             print("Nilai untuk mata kuliah", matkul,":", nilai, "( grade", grade, ")")
             #melakukan perhitung untuk mendapatkan hasil ip
@@ -75,4 +80,4 @@ print("NIM :", nim)
 #Mencetak Total SKS
 print("Total SKS :", totalsks)
 #Mencetak nilai Index Prestasi dibagi total sks
-print("Index prestasi :", round(ip/totalsks, 2))
+print("Index prestasi :", round(ip/totalsks, 2)) #fungsi round ini untuk membulatkan suatu bilangan desimal berkoma
